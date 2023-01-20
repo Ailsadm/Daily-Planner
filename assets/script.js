6680792571c453714c9e38d5aef409a822af3699
Ailsadm <ailsamcgowan@hotmail.co.uk>
2023-01-20 19:46:24 +0000

    first html timeblock added

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,14 @@ setInterval(
         $("#date").text(moment().format("DDD MMM, YYYY hh:mm:ss"))
 
     }, 1000)
+
+let $plannerItems = $(".plannerItem");
+let $dailyPlannerRow = $(".dailyPlannerRow");
+
+function handleItemSubmission(event){
+    let plannerItem = $('input[name="planner-input"]').val();
+
+    let dailyPlannerItem = $("<li>")
+}
+
+$dailyPlannerRow.on("submit", handleItemSubmission);
